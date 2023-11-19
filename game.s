5ec5a5d7deba5b86cd64ba0f4b4a7dd1816f45ff
Igor Cieply <igoracieply@outlook.com>
2023-11-19 15:19:31 -0600

    Added ability to print map to console

--- game.s
+++ game.s
@@ -19,13 +19,26 @@ main:
     # Store return address on stack
     addi $sp, $sp, -4
     sw $ra, 0($sp)
+	
+	jal copy_map
+	or $0, $0, $0
+	
+	jal print_map
+	or $0, $0, $0
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
 
-    addi $sp, $sp, -4
+    jr $ra
+    or $0, $0, $0
+
+copy_map:
+	addi $sp, $sp, -4
     sw $s0, 0($sp)
 
     addi $sp, $sp, -4
     sw $s1, 0($sp)
-    # Copy static map data to dynamic map
+	
     # Address of static map byte array in data segment
     lui $s0, 0x1000
     ori $s0, 0x2000
@@ -48,10 +61,58 @@ main:
         addi $t4, $0, 64
         bne $t0, $t4, copy_static_to_dyn_loop
         or $0, $0, $0
+		
+	lw $s1, 0($sp)
+	addi $sp, $sp, 4
+	
+	lw $s0, 0($sp)
+	addi $sp, $sp, 4
 
-    jr $ra
-    or $0, $0, $0
+	jr $ra
+	or $0, $0, $0
 
 print_map:
+	addi $sp, $sp, -4
+    sw $s0, 0($sp)
+	
+	# Address of dynamic map byte array in data segment
+    lui $s0, 0x1000
+    ori $s0, 0x3000
+	
+	# Values
+	add $t0, $0, $0 # Byte index
+	add $t5, $0, $0 # New line counter
+	print_dyn_map_loop:
+		add $t1, $s0, $t0 # Construct full character address
+        lb $a0, 0($t1) # Pull character from dynamic map
+        or $0, $0, $0
+		
+		# Print character
+		addi $v0, $0, 11
+		syscall
+		
+		# If its the 8th byte, print a new line character as well
+		addi $t5, $t5, 1
+		addi $t6, $0, 8
+		bne $t5, $t6, skip_if_not_8th
+		addi $v0, $0, 11
+		addi $a0, $0, 10
+		syscall
+		
+		add $t5, $0, $0
+		
+		skip_if_not_8th:
+		
+		# Increment index
+		addi $t0, $t0, 1
+		
+		# Exit loop if index reaches 64
+        addi $t4, $0, 64
+        bne $t0, $t4, print_dyn_map_loop
+        or $0, $0, $0
+	
+	lw $s0, 0($sp)
+	addi $sp, $sp, 4
+	
     jr $ra
     or $0, $0, $0
