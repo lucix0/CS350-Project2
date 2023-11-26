34a657951983d83d5daf071324fc347b4c6c811f
Lucyna Cieply <igoracieply@outlook.com>
2023-11-26 12:41:35 -0600

    Movement almost done

--- game.s
+++ game.s
@@ -13,6 +13,11 @@
 .data 0x10003000
 .space 64 # Reserved space for the map
 
+# Variables
+.data 0x10004000
+.word 0 # X position
+.word 7 # Y position
+
 .text
 .globl main
 main:
@@ -37,7 +42,7 @@ initialize_game:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
 
-    jal copy_map
+    jal update_map
     or $0, $0, $0
 
     lw $ra, 0($sp)
@@ -68,17 +73,59 @@ game_loop:
         or $0, $0, $0
         add $s1, $0, $v0
 
-        # Update game state
-
-        # Should game terminate by keypress?
+        # Is it w?
+        addi $t0, $0, 119 # 'w' ASCII code
+        bne $s1, $t0, skip_not_w
+        or $0, $0, $0
+        # Move player up 1 unit
+        addi $a0, $0, 0
+        addi $a1, $0, -1
+        jal move_player
+        or $0, $0, $0
+    skip_not_w:
+        # Is it a?
+        addi $t0, $0, 97 # 'a' ASCII code
+        bne $s1, $t0, skip_not_a
+        or $0, $0, $0
+        # Move player left 1 unit
+        addi $a0, $0, -1
+        addi $a1, $0, 0
+        jal move_player
+        or $0, $0, $0
+    skip_not_a:
+        # Is it s?
+        addi $t0, $0, 115 # 's' ASCII code
+        bne $s1, $t0, skip_not_s
+        or $0, $0, $0
+        # Move player down 1 unit
+        addi $a0, $0, 0
+        addi $a1, $0, 1
+        jal move_player
+        or $0, $0, $0
+    skip_not_s:
+        # Is it d?
+        addi $t0, $0, 100 # 'd' ASCII code
+        bne $s1, $t0, skip_not_d
+        or $0, $0, $0
+        # Move player right 1 unit
+        addi $a0, $0, 1
+        addi $a1, $0, 0
+        jal move_player
+        or $0, $0, $0
+    skip_not_d:
+        # Is it q?
         addi $t0, $0, 113 # q ASCII code
-        bne $s1, $t0, dont_terminate_from_key
+        bne $s1, $t0, skip_none
         or $0, $0, $0
         addi $s0, $0, 1
-    dont_terminate_from_key:
+    skip_none:
         # Run loop again if shouldn't terminate
         beq $s0, $0, game_loop_start
         or $0, $0, $0
+
+    # Update map
+    jal update_map
+    or $0, $0, $0
 
     lw $s1, 0($sp)
     addi $sp, $sp, 4
@@ -100,13 +147,104 @@ get_user_input:
     jr $ra
     or $0, $0, $0
 
-copy_map:
+# Name: move_player
+# Arguments: a0 - Delta-X, a1 - DeltaY
+# Return: None
+move_player:
     addi $sp, $sp, -4
     sw $s0, 0($sp)
 
     addi $sp, $sp, -4
     sw $s1, 0($sp)
+
+    addi $sp, $sp, -4
+    sw $s2, 0($sp)
+
+    addi $sp, $sp, -4
+    sw $s3, 0($sp)
+
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    # Address of player data
+    lui $s0, 0x1000
+    addi $s0, $s0, 0x4000
+
+    # Load X Position
+    lw $s1, 0($s0)
+    or $0, $0, $0
+    # Load Y Position
+    lw $s2, 4($s0)
+    or $0, $0, $0
+
+    # Apply change
+    # X-Position
+    add $s1, $s1, $a0
+    # Y-Position
+    add $s2, $s2, $a1
+
+    # Is the new player location open?
+    # If not, don't move player
+    # Get character at position
+    add $a0, $0, $s1
+    add $a1, $0, $s2
+    jal position_to_offset
+    or $0, $0, $0
+    add $t0, $0, $v0
+
+    # Construct address of map
+    lui $t2, 0x1000
+    addi $t2, $t2, 0x3000
+
+    # Get character at position
+    add $t2, $t2, $t0
+    lb $s3, 0($t2)
+    or $0, $0, $0
+
+    # If its not a wall, change position
+    # Otherwise, don't change position
+    addi $t3, $0, 35 # '#' ASCII code
+    beq $s3, $t3, skip_wall
+    or $0, $0, $0
+    sw $s1, 0($s0)
+    sw $s2, 4($s0)
+
+skip_wall:
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+
+    lw $s3, 0($sp)
+    addi $sp, $sp, 4
+
+    lw $s2, 0($sp)
+    addi $sp, $sp, 4
+
+    lw $s1, 0($sp)
+    addi $sp, $sp, 4
+
+    lw $s0, 0($sp)
+    addi $sp, $sp, 4
+
+    jr $ra
+    or $0, $0, $0
+
+update_map:
+    addi $sp, $sp, -4
+    sw $s0, 0($sp)
+
+    addi $sp, $sp, -4
+    sw $s1, 0($sp)
+
+    addi $sp, $sp, -4
+    sw $s2, 0($sp)
+
+    addi $sp, $sp, -4
+    sw $s3, 0($sp)
+
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
     
+    # Copy static elements into the dynamic map (Walls and Exit Gate)
     # Address of static map byte array in data segment
     lui $s0, 0x1000
     ori $s0, 0x2000
@@ -120,6 +258,15 @@ copy_map:
         lb $t3, 0($t1) # Pull character from static map
         or $0, $0, $0
 
+        # What is the character?
+        # If its not static, place a space
+        # Otherwise, just place the given character
+        addi $t4, $0, 83 # 'S' ASCII code
+        bne $t4, $t3, skip_static
+        or $0, $0, $0
+        addi $t3, $0, 32 # ' ' ASCII code
+
+    skip_static:
         add $t2, $s1, $t0 # Construct full character address
         sb $t3, 0($t2) # Put character into dynamic map
 
@@ -129,12 +276,57 @@ copy_map:
         addi $t4, $0, 64
         bne $t0, $t4, copy_static_to_dyn_loop
         or $0, $0, $0
-        
+
+    # Place player into map
+    # Address of player data
+    lui $t0, 0x1000 
+    ori $t0, 0x4000
+    lb $s2, 0($t0) # X-Position
+    or $0, $0, $0
+    lb $s3, 4($t0) # Y-Position
+    or $0, $0, $0
+
+    add $a0, $0, $s2
+    add $a1, $0, $s3
+    jal position_to_offset
+    or $0, $0, $0
+    add $t1, $0, $v0
+
+    # Place player at this address
+    lui $t0, 0x1000 
+    ori $t0, 0x3000
+    add $t0, $t0, $t1
+    addi $t2, $0, 83 # 'S' ASCII code
+    sw $t2, 0($t0)
+
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+
+    lw $s3, 0($sp)
+    addi $sp, $sp, 4
+    
+    lw $s2, 0($sp)
+    addi $sp, $sp, 4
+
     lw $s1, 0($sp)
     addi $sp, $sp, 4
     
     lw $s0, 0($sp)
     addi $sp, $sp, 4
+
+    jr $ra
+    or $0, $0, $0
+
+# Name: position_to_offset
+# Arguments: a0 - X, a1 - Y
+# Return: v0 - Offset
+position_to_offset:
+    # Offset = Y * 8 + X
+    addi $t0, $0, 8
+    mult $a1, $t0
+    mflo $t0
+    or $0, $0, $0
+    add $v0, $a0, $t0
 
     jr $ra
     or $0, $0, $0
